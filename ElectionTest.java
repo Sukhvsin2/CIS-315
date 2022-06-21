1c369c506a0b2aaef2df37a1db8081193e82ff93
Sukhvinder Singh <46042060+Sukhvsin2@users.noreply.github.com>
2022-06-21 11:44:51 -0500

    Add files via upload

--- ElectionTest.java
+++ ElectionTest.java
@@ -1,14 +1,20 @@
 public class ElectionTest
 {
     static Election createElection(){
-        Election obj = new Election();
-        obj.addACandidate("Donald", "Trump", 'R', true);
-        obj.addACandidate("Ben", "Carson", 'R', false);
-        obj.addACandidate("Jeb", "Bush", 'R', false);
-        obj.addACandidate("Hillary", "Clinton", 'D', true);
-        obj.addACandidate("Bernie", "Sanders", 'D', false);
-        obj.addACandidate("Jim", "Webb", 'D', false);
-        return obj;
+        Election el = new Election();
+        Candidate obj = new Candidate("Donald", "Trump", 'R', true);
+        Candidate obj1 = new Candidate("Ben", "Carson", 'R', false);
+        Candidate obj2 = new Candidate("Jeb", "Bush", 'R', false);
+        Candidate obj3 = new Candidate("Hillary", "Clinton", 'D', true);
+        Candidate obj4 = new Candidate("Bernie", "Sanders", 'D', false);
+        Candidate obj5 = new Candidate("Jim", "Webb", 'D', false);
+        el.addACandidate(obj);
+        el.addACandidate(obj1);
+        el.addACandidate(obj2);
+        el.addACandidate(obj3);
+        el.addACandidate(obj4);
+        el.addACandidate(obj5);
+        return el;
     }
 	public static void main(String[] args) {
 	    Election obj = createElection();
